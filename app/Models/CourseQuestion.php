874dbf3b6f8bdf7db2d570ea7725aee7c2708705
Arlann <handikaarlan@gmail.com>
2024-11-22 20:31:39 +0700

    feat(models): add relationships and traits to course models

--- app/Models/CourseQuestion.php
+++ app/Models/CourseQuestion.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CourseQuestion extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [
+        'id',
+    ];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class, 'course_id');
+    }
+
+    public function answer()
+    {
+        return $this->hasMany(CourseAnswer::class, 'question_id', 'id');
+    }
 }
