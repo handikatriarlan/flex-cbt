fd29e777a8f9f723a5b3330f3cc83e35d1e64c31
Arlann <handikaarlan@gmail.com>
2024-11-27 21:11:09 +0700

    feat(learning): adds next unanswered question logic

--- app/Http/Controllers/LearningController.php
+++ app/Http/Controllers/LearningController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\StudentAnswer;
+use App\Models\CourseQuestion;
 use Illuminate\Support\Facades\Auth;
 
 class LearningController extends Controller
@@ -11,6 +13,27 @@ class LearningController extends Controller
     {
         $user = Auth::user();
         $my_courses = $user->courses()->with('category')->orderBy('id', 'DESC')->get();
+
+        foreach ($my_courses as $course) {
+            $totalQuestionsCount = $course->questions()->count();
+
+            $answeredQuestionsCount = StudentAnswer::where('user_id', $user->id)
+                ->whereHas('question', function ($query) use ($course) {
+                    $query->where('course_id', $course->id);
+                })->distinct()->count('question_id');
+
+            if ($answeredQuestionsCount < $totalQuestionsCount) {
+                $firstUnansweredQuestion = CourseQuestion::where('course_id', $course->id)
+                    ->whereNotIn('id', function ($query) use ($user) {
+                        $query->select('question_id')->from('student_answers')
+                            ->where('user_id', $user->id);
+                    })->orderBy('id', 'ASC')->first();
+
+                $course->nextQuestionId = $firstUnansweredQuestion ? $firstUnansweredQuestion->id : null;
+            } else {
+                $course->nextQuestionId = null;
+            }
+        }
 
         return view('student.courses.index', [
             'my_courses' => $my_courses,
--- resources/views/student/courses/index.blade.php
+++ resources/views/student/courses/index.blade.php
@@ -210,7 +210,7 @@
                         @endif
 
                         <div class="flex shrink-0 w-[120px] items-center">
-                            <a href="learning.html"
+                            <a href="{{ route('dashboard.learning.course', ['course' => $course->id, 'question' => $course->nextQuestionId]) }}"
                                 class="w-full h-[41px] p-[10px_20px] bg-[#6436F1] rounded-full font-bold text-sm text-white transition-all duration-300 hover:shadow-[0_4px_15px_0_#6436F14D] text-center">Start
                                 Test</a>
                         </div>
