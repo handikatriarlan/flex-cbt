93d6d93570d6ef87e3cfce1173069b3bc2365bdb
Arlann <handikaarlan@gmail.com>
2024-11-27 13:54:40 +0700

    feat(student-course): displays user courses on index page

--- app/Http/Controllers/LearningController.php
+++ app/Http/Controllers/LearningController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LearningController extends Controller
 {
     public function index()
     {
-        return view('student.courses.index');
+        $user = Auth::user();
+        $my_courses = $user->courses()->with('category')->orderBy('id', 'DESC')->get();
+
+        return view('student.courses.index', [
+            'my_courses' => $my_courses,
+        ]);
     }
 }
