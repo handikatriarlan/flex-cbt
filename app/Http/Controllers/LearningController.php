a62f920af4c839fe57b5e3fa516ea39728fe526e
Arlann <handikaarlan@gmail.com>
2024-12-04 14:06:58 +0700

    feat(learning): add learning page and route logic

--- app/Http/Controllers/LearningController.php
+++ app/Http/Controllers/LearningController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use Illuminate\Http\Request;
 use App\Models\StudentAnswer;
 use App\Models\CourseQuestion;
@@ -20,12 +21,12 @@ class LearningController extends Controller
             $answeredQuestionsCount = StudentAnswer::where('user_id', $user->id)
                 ->whereHas('question', function ($query) use ($course) {
                     $query->where('course_id', $course->id);
-                })->distinct()->count('question_id');
+                })->distinct()->count('course_question_id');
 
             if ($answeredQuestionsCount < $totalQuestionsCount) {
                 $firstUnansweredQuestion = CourseQuestion::where('course_id', $course->id)
                     ->whereNotIn('id', function ($query) use ($user) {
-                        $query->select('question_id')->from('student_answers')
+                        $query->select('course_question_id')->from('student_answers')
                             ->where('user_id', $user->id);
                     })->orderBy('id', 'ASC')->first();
 
@@ -37,6 +38,28 @@ class LearningController extends Controller
 
         return view('student.courses.index', [
             'my_courses' => $my_courses,
+        ]);
+    }
+
+    public function learning(Course $course, $question)
+    {
+        $user = Auth::user();
+
+        $isEnrolled = $user->courses()
+            ->where('course_id', $course->id)
+            ->exists();
+
+        if (!$isEnrolled) {
+            abort(404);
+        }
+
+        $currentQuestion = CourseQuestion::where('course_id', $course->id)
+            ->where('id', $question)
+            ->firstOrFail();
+
+        return view('student.courses.learning', [
+            'course' => $course,
+            'question' => $currentQuestion,
         ]);
     }
 }
